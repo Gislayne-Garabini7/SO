cea9e721ccfa7879ed4647c6a1d0739381cfd4a3
Luísa <149273460+luis4carla@users.noreply.github.com>
2023-11-20 22:41:27 -0400

    LeituraV2.c
    versão atualizada com libCurl

--- LeituraV2.c
+++ LeituraV2.c
@@ -5,89 +5,82 @@
 #include <string.h>
 #include <errno.h>
 #include <sys/stat.h>
+#include <curl/curl.h>
 
-static const char *caminhoArquivo = "Caminho do Arquivo"; // Ex:C:\Users\Fulana\Desktop\SO\exemplo.txt
+static const char *git_repo_url = "https://raw.githubusercontent.com/usuario/repositorio/master/exemplo.txt";
+static const char *caminho_arquivo = "/caminho/para/seu/repositorio/git/exemplo.txt"; // Ex:C:\Users\Fulana\Desktop\SO\exemplo.txt
 
-
-// Função para obter atributos do arquivo (FUSE)
-static int myfs_obter_atributos(const char *caminho, struct stat *stbuf) {
+static int myfs_obter_atributos(const char *caminho, struct stat *stbuf) { // Função para obter atributos do arquivo (FUSE)
     int resultado = 0;
 
-    // Obtém informações do arquivo usando stat
-    if (stat(caminho_arquivo, stbuf) == -1) {
-        resposta = -errno; // Erro ao obter informações do arquivo
-        return resposta;
+    if (stat(caminho_arquivo, stbuf) == -1) { // Obtém informações do arquivo usando stat
+        resultado = -errno; // Erro ao obter informações do arquivo
+        return resultado;
     }
 
-    return resposta;
+    return resultado;
 }
 
-/*
-static int myfs_getattr(const char *caminho, struct stat *stbuf) {
-    int resposta = 0;
-
-    memset(stbuf, 0, sizeof(struct stat));
-
-    if (strcmp(caminho, "/exemplo.txt") == 0) { //confere se é o arquivo correto, define as permissões, tamanho e tipo;
-        stbuf->st_mode = S_IFREG | 0444; //código que define como somente leitura, 0666 para leitura e gravação, depende do que quer
-/*Ex:   4: Permissão de leitura (Read)
-        2: Permissão de gravação (Write)
-        1: Permissão de execução (Execute)
-        Os números 4, 2, e 1 correspondem aos bits de permissão, e você os soma para obter a combinação desejada. Portanto, 0444 significa:
-
-        4 (leitura) para o usuário (dono do arquivo),
-        4 (leitura) para o grupo,
-        4 (leitura) para outros.
-        Se você deseja adicionar permissões de escrita, você adiciona 2 (permissão de gravação). Se deseja permissões de leitura e gravação para o usuário, você usaria 0644:
-
-        6 (leitura e gravação) para o usuário,
-        4 (leitura) para o grupo,
-        4 (leitura) para outros.
-        Se quiser dar permissões de leitura e escrita para todos, você usaria 0666:
-
-        6 (leitura e gravação) para o usuário,
-        6 (leitura e gravação) para o grupo,
-        6 (leitura e gravação) para outros.*
-
-        stbuf->st_nlink = 1; //Definição do número de links para o arquivo como 1
-        //stbuf->st_size = 1024; // Substitua pelo tamanho real do seu arquivo
-    } else {
-        resposta = -ENOENT; // Arquivo não encontrado
-    }
-
-    return resposta;
-}
-*/
-static int myfs_ler_arquivo(const char *caminho, char *buf, size_t tamanho, off_t offset, struct fuse_file_info *fi) {
+static int myfs_ler_arquivo(const char *caminho, char *buf, size_t tamanho, off_t offset, struct fuse_file_info *fi) { // Função para ler o conteúdo do arquivo (FUSE)
     (void) fi;
 
-    if (strcmp(caminho, "/exemplo.txt") != 0) {// Verifica se o caminho corresponde ao arquivo
+    if (strcmp(caminho, "/exemplo") != 0) { // Se o caminho for o arquivo fictício
         return -ENOENT; // Arquivo não encontrado
     }
 
-    FILE *arquivo = fopen(caminhoArquivo, "r");// Abre o arquivo em modo de leitura
-    // "r" para leitura, "w" para escrita, precisa confirmar
-    if (arquivo == NULL) {
-        return -errno; // Erro ao abrir o arquivo
+    // Usando libcurl para baixar o conteúdo do arquivo remoto
+    CURL *curl;
+    CURLcode res; // res de resultado
+
+
+    FILE *arquivo_local = fopen(caminho_arquivo, "wb"); // Abre o arquivo local para escrita
+    if (!arquivo_local) {
+        return -errno; // Erro ao abrir o arquivo local
     }
 
-    if (fseek(arquivo, offset, SEEK_SET) == -1) {// Move o cursor para a posição especificada pelo offset
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, git_repo_url); // Configura a URL do arquivo no repositório Git
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, arquivo_local); // Configura a saída para o arquivo local
+        res = curl_easy_perform(curl); // Executa a requisição
+
+        curl_easy_cleanup(curl); // Finaliza a sessão libcurl
+    } else {
+        fclose(arquivo_local);
+        curl_global_cleanup();
+        return -EIO; // Erro ao inicializar libcurl
+    }
+
+    curl_global_cleanup(); // Finaliza a biblioteca libcurl
+
+    fclose(arquivo_local);
+
+    FILE *arquivo = fopen(caminho_arquivo, "r"); // Arquivo local atualizado, chamada de função de leitura
+    if (!arquivo) {
+        return -errno; // Erro ao abrir o arquivo local
+    }
+
+    if (fseek(arquivo, offset, SEEK_SET) == -1) { // Move o cursor para a posição especificada pelo offset
         fclose(arquivo);
         return -errno; // Erro ao mover o cursor
     }
 
-    size_t bytes_lidos = fread(buf, 1, tamanho, arquivo);// Lê até 'tamanho' bytes do arquivo para o buffer
+    size_t bytes_lidos = fread(buf, 1, tamanho, arquivo); // Lê até 'tamanho' bytes do arquivo para o buffer
     fclose(arquivo);
 
     return bytes_lidos;
 }
 
-static struct fuse_operations myfs_operacoes = {// Operações FUSE utilizadas
+// Estrutura que define as operações FUSE utilizadas
+static struct fuse_operations myfs_operacoes = {
     .getattr = myfs_obter_atributos,
     .read = myfs_ler_arquivo,
-    // Outras funções do FUSE aqui
+    // Outras funções do FUSE aqui...
 };
 
-int main(int argc, char *argv[]) {// Inicia o FUSE com as operações definidas
+int main(int argc, char *argv[]) {
+
     return fuse_main(argc, argv, &myfs_operacoes, NULL);
+
 }
