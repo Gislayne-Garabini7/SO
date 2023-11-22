7c4b2f8c1cef247a3030aa49e4c57013fbc94463
Luísa <149273460+luis4carla@users.noreply.github.com>
2023-11-22 16:48:35 -0400

    Função LeituraV2.c

--- LeituraV2.c
+++ LeituraV2.c
@@ -1,86 +1,39 @@
-#define FUSE_USE_VERSION 31
+/*#define FUSE_USE_VERSION 31
 #include <fuse.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-#include <sys/stat.h>
-#include <curl/curl.h>
+#include <sys/stat.h>*/
 
-static const char *git_repo_url = "https://raw.githubusercontent.com/usuario/repositorio/master/exemplo.txt";
-static const char *caminho_arquivo = "/caminho/para/seu/repositorio/git/exemplo.txt"; // Ex:C:\Users\Fulana\Desktop\SO\exemplo.txt
-
-static int myfs_obter_atributos(const char *caminho, struct stat *stbuf) { // Função para obter atributos do arquivo (FUSE)
-    int resultado = 0;
-
-    if (stat(caminho_arquivo, stbuf) == -1) { // Obtém informações do arquivo usando stat
-        resultado = -errno; // Erro ao obter informações do arquivo
-        return resultado;
+// Função de leitura de arquivo
+static int myfs_read(const char *MyFS, char *buf, size_t size, off_t offset, struct fuse_file_info *fi){
+    (void)fi;
+    
+    int fileIndex = -1; //Valor pré definido para não encontrado
+    for (int i = 0; i < myFileSystem.numFiles; i++){ // Procuramos o índice do arquivo no sistema
+        if (strcmp(myFileSystem.files[i].filename, MyFS) == 0)
+        {
+            fileIndex = i;
+            break;
+        }
     }
 
-    return resultado;
-}
-
-static int myfs_ler_arquivo(const char *caminho, char *buf, size_t tamanho, off_t offset, struct fuse_file_info *fi) { // Função para ler o conteúdo do arquivo (FUSE)
-    (void) fi;
-
-    if (strcmp(caminho, "/exemplo") != 0) { // Se o caminho for o arquivo fictício
+    if (fileIndex == -1){ // Se o arquivo não foi encontrado, retornamos erro
         return -ENOENT; // Arquivo não encontrado
     }
 
-    // Usando libcurl para baixar o conteúdo do arquivo remoto
-    CURL *curl;
-    CURLcode res; // res de resultado
-
-
-    FILE *arquivo_local = fopen(caminho_arquivo, "wb"); // Abre o arquivo local para escrita
-    if (!arquivo_local) {
-        return -errno; // Erro ao abrir o arquivo local
+    if (offset >= myFileSystem.files[fileIndex].size){ // Se a posição de leitura está além do final do arquivo, não há nada para ler
+        return 0; // Nada a ser lido além do final do arquivo
     }
 
-    curl_global_init(CURL_GLOBAL_DEFAULT);
-    curl = curl_easy_init();
-    if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, git_repo_url); // Configura a URL do arquivo no repositório Git
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, arquivo_local); // Configura a saída para o arquivo local
-        res = curl_easy_perform(curl); // Executa a requisição
+    size_t bytesRead = size; // Inicialmente, lemos a quantidade total especificada (size), esse é o tamanho fornecido para o buffer
 
-        curl_easy_cleanup(curl); // Finaliza a sessão libcurl
-    } else {
-        fclose(arquivo_local);
-        curl_global_cleanup();
-        return -EIO; // Erro ao inicializar libcurl
+    if (offset + size > myFileSystem.files[fileIndex].size){ // Se a leitura ultrapassar o final do arquivo, ajustamos o número de bytes a ser lido
+        bytesRead = myFileSystem.files[fileIndex].size - offset;
     }
 
-    curl_global_cleanup(); // Finaliza a biblioteca libcurl
+    memcpy(buf, myFileSystem.files[fileIndex].content + offset, bytesRead); // Copiamos os dados do arquivo para o buffer fornecido
 
-    fclose(arquivo_local);
-
-    FILE *arquivo = fopen(caminho_arquivo, "r"); // Arquivo local atualizado, chamada de função de leitura
-    if (!arquivo) {
-        return -errno; // Erro ao abrir o arquivo local
-    }
-
-    if (fseek(arquivo, offset, SEEK_SET) == -1) { // Move o cursor para a posição especificada pelo offset
-        fclose(arquivo);
-        return -errno; // Erro ao mover o cursor
-    }
-
-    size_t bytes_lidos = fread(buf, 1, tamanho, arquivo); // Lê até 'tamanho' bytes do arquivo para o buffer
-    fclose(arquivo);
-
-    return bytes_lidos;
-}
-
-// Estrutura que define as operações FUSE utilizadas
-static struct fuse_operations myfs_operacoes = {
-    .getattr = myfs_obter_atributos,
-    .read = myfs_ler_arquivo,
-    // Outras funções do FUSE aqui...
-};
-
-int main(int argc, char *argv[]) {
-
-    return fuse_main(argc, argv, &myfs_operacoes, NULL);
-
+    return bytesRead; // Retornamos o número de bytes lidos
 }
